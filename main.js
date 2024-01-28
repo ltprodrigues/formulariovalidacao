aa39c8ec0ea5fedd081893a1b936dec2e1e3d57a
Leticia Rodrigues <leticiateixeirar@gmail.com>
2024-01-28 12:34:52 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('form-calcule');
+
+function validarFormulario() {
+    var firstNumber = parseFloat(document.getElementById('firstNumber').value);
+    var secondNumber = parseFloat(document.getElementById('secondNumber').value);
+
+    if(secondNumber > firstNumber){
+        alert('Invalido! Segundo numero e maior que o primeiro');
+    }else{
+        alert('Valido! O segundo numero deve ser maior que o primeiro.');
+    }
+}
