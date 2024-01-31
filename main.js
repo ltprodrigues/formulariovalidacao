db7fb622a233434a19bd1d803ece379f3aeaaa26
Leticia Rodrigues <leticiateixeirar@gmail.com>
2024-01-30 19:57:26 -0500

    -

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ function validarFormulario() {
     var firstNumber = parseFloat(document.getElementById('firstNumber').value);
     var secondNumber = parseFloat(document.getElementById('secondNumber').value);
 
-    if(secondNumber > firstNumber){
-        alert('Invalido! Segundo numero e maior que o primeiro');
+    if(secondNumber < firstNumber){
+        alert('Invalido! Segundo numero deve ser maior que o primeiro');
     }else{
-        alert('Valido! O segundo numero deve ser maior que o primeiro.');
+        alert('Valido! O segundo numero eh maior que o primeiro.');
     }
 }
